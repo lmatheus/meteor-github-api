d2741854c19d0250b2acc7d7e515debefd73e95c
Bruz Marzolf <bruz@bruzilla.com>
2014-09-15 08:43:46 -0700

    Update for Meteor 0.9.0

--- package.js
+++ package.js
@@ -1,10 +1,12 @@
 Package.describe({
-  summary: 'NodeJS wrapper for the GitHub API'
+  summary: 'NodeJS wrapper for the GitHub API',
+  version: "0.1.11"
 });
 
 Npm.depends({github: '0.1.10'});
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR-CORE@0.9.0-atm");
   api.export('GitHub');
 
   api.add_files('github-api.js', 'server');
