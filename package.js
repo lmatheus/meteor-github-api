6dc6d3061813f479bb53727e478ef729c9209d2e
Marat Safin <m.safin@softeam.co>
2015-05-31 11:02:21 +0300

    Update the node-github version to 0.2.4

--- package.js
+++ package.js
@@ -2,10 +2,10 @@ Package.describe({
   git: "https://github.com/bruz/meteor-github-api.git",
   name: "bruz:github-api",
   summary: 'NodeJS wrapper for the GitHub API',
-  version: "0.2.2_2"
+  version: "0.2.4_1"
 });
 
-Npm.depends({github: '0.2.2'});
+Npm.depends({github: '0.2.4'});
 
 Package.on_use(function (api) {
   api.versionsFrom("METEOR-CORE@0.9.0-atm");
