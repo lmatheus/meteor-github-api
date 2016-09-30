5ed6145d2803896d31f008f5285b5020538785c2
Luis Matheus <ing.luis.matheus@gmail.com>
2016-09-30 15:07:15 -0500

    updated version to 3.1.1

--- package.js
+++ package.js
@@ -2,13 +2,13 @@ Package.describe({
   git: "https://github.com/lmatheus/meteor-github-api.git",
   name: "lmatheus:github-api",
   summary: 'NodeJS wrapper for the GitHub API',
-  version: "2.4.0"
+  version: "3.1.1"
 });
 
-Npm.depends({github: '2.4.0'});
+Npm.depends({github: '3.1.1'});
 
 Package.on_use(function (api) {
-  api.versionsFrom("1.4.0.1");
+  api.versionsFrom("1.4.1");
   api.use(['underscore'], ['client', 'server']);
   api.export('GitHub');
 
